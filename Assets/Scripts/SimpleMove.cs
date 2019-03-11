2998d1b0d671be0e9e4cf7f12d0ee7407e1d89b4
ana-juh <anajuliaiftm@gmail.com>
2019-03-11 08:39:42 -0300

    Incluindo Limites na tela do jogo

--- Assets/Scripts/SimpleMove.cs
+++ Assets/Scripts/SimpleMove.cs
@@ -6,14 +6,15 @@ public class SimpleMove : MonoBehaviour
 {
     [SerializeField]
     private float SpeedFactor = 1f;
-       [SerializeField]
-     private float LimitTop = 4f;
-        [SerializeField]
+    [SerializeField]
+    private float LimitTop = 4f;
+    [SerializeField]
     private float LimitBottom = -4f;
-        [SerializeField]
+    [SerializeField]
     private float LimitLeft = -8f;
-       [SerializeField]
-     private float limitRight = 8f;
+    [SerializeField]
+    private float limitRight = 8f;
+    
     void Start()
     {
     
@@ -22,15 +23,26 @@ public class SimpleMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       float horizontalInput = Input.GetAxisRaw("Horizontal");
-       float verticalInput = Input.GetAxisRaw("Vertical");
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
 
-       Vector3 velocity = new Vector3 (horizontalInput, verticalInput, 0f) * SpeedFactor;
+        Vector3 velocity = new Vector3 (horizontalInput, verticalInput, 0f) * SpeedFactor;
 
-       transform.position = transform.position + velocity * Time.deltaTime;
+        transform.position = transform.position + velocity * Time.deltaTime;
 
-       if (transform.position.y < LimitBottom){
-           transform.position = new Vector3 (transform.position.x, LimitBottom, transform.position.z);
-       }
+        if (transform.position.y < LimitBottom){
+            transform.position = new Vector3 (transform.position.x, LimitBottom, transform.position.z);
+        }
+        if (transform.position.y > LimitTop){
+            transform.position = new Vector3 (transform.position.x, LimitTop, transform.position.z);
+        }
+        if (transform.position.x < LimitLeft){
+            transform.position = new Vector3 (LimitLeft, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > limitRight){
+            transform.position = new Vector3 (limitRight, transform.position.y, transform.position.z);
+        }
     }
-}
+} 
+
+
