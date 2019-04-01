a780d56f69e786d7a8b06b19300d907205a6329d
ana-juh <anajuliaiftm@gmail.com>
2019-04-01 08:39:46 -0300

    Tocando som da moeda com AudioManager e AudioSFX

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -5,14 +5,16 @@ using UnityEngine;
 public class AudioManager : MonoBehaviour
 {
     
-    void Start()
-    {
-        
-    }
-
    
-    void Update()
-    {
-        
-    }
+   public static AudioManager instance;
+
+   public AudioSFX audioSFX;
+   void Awake () {
+       if (instance == null) {
+           instance = this;
+       }
+   }
+   public void PlaySoundCoinPickup(GameObject obj) {
+       AudioSource.PlayClipAtPoint(audioSFX.coinPickup, obj.transform.position);
+   }
 }
--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -4,20 +4,10 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-    
-    void Start()
-    {
-        
-    }
-
-    
-    void Update()
-    {
-        
-    }
-    void OnTriggerEnter2D(Collider2D other){
+        void OnTriggerEnter2D(Collider2D other){
         if (other.gameObject.CompareTag("Coin")){
             SFXManager.instance.ShowCoinParticles(other.gameObject);
+            AudioManager.instance.PlaySoundCoinPickup(other.gameObject);
             Destroy(other.gameObject);
         }
     }
